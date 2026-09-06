10d7e231836a24762411921aaa05242dcd8b8489
Christopher Raleigh <craleigh318@comcast.net>
2026-09-05 17:51:20 -0700

    Add SQLite

--- 21WithEmilyWeb.Api/Program.cs
+++ 21WithEmilyWeb.Api/Program.cs
@@ -1,16 +1,29 @@
 using _21WithEmilyWeb.Api.Data;
 using _21WithEmilyWeb.Api.Services;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Connect to database.
+if (builder.Environment.IsProduction())
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString));
+}
+else
+{
+    var connection = new SqliteConnection("Data Source=:memory:");
+    connection.Open();
+
+    builder.Services.AddSingleton(connection);
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite(connection));
+}
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
 builder.Services.AddScoped<GameService>();
-
 builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
